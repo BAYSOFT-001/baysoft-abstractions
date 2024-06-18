5053e65e223920e10315fe872cf7d8cb66a0a0f0
Italo Silveira <italobrian@gmail.com>
2024-06-18 11:41:34 -0300

    v8.0.4

--- src/BAYSOFT.Abstractions/Infrastructures/Data/Writer.cs
+++ src/BAYSOFT.Abstractions/Infrastructures/Data/Writer.cs
@@ -1,6 +1,9 @@
 ﻿using BAYSOFT.Abstractions.Core.Domain.Entities;
 using BAYSOFT.Abstractions.Core.Domain.Interfaces.Infrastructures.Data;
+using BAYSOFT.Abstractions.EntityFrameworkCore.Extensions;
 using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,27 +17,22 @@ namespace BAYSOFT.Abstractions.Infrastructures.Data
         {
             Context = context;
         }
-
         public virtual IQueryable<TEntity> Query<TEntity>() where TEntity : DomainEntity
         {
             return Context.Set<TEntity>().AsQueryable();
         }
-
         public virtual void Add<TEntity>(TEntity entity) where TEntity : DomainEntity
         {
             Context.Set<TEntity>().Add(entity);
         }
-
         public virtual Task AddAsync<TEntity>(TEntity entity) where TEntity : DomainEntity
         {
             return Context.Set<TEntity>().AddAsync(entity).AsTask();
         }
-
         public virtual void AddRange<TEntity>(params TEntity[] entities) where TEntity : DomainEntity
         {
             Context.Set<TEntity>().AddRange(entities);
         }
-
         public virtual Task AddRangeAsync<TEntity>(params TEntity[] entities) where TEntity : DomainEntity
         {
             return Context.Set<TEntity>().AddRangeAsync(entities);
@@ -51,10 +49,33 @@ namespace BAYSOFT.Abstractions.Infrastructures.Data
         {
             return Context.SaveChanges();
         }
-
         public virtual Task<int> CommitAsync(CancellationToken cancellationToken = default)
         {
             return Context.SaveChangesAsync(cancellationToken);
-        }
-    }
+		}
+		public IQueryable<TEntity> FromSqlInterpolated<TEntity>(FormattableString sql) where TEntity : DomainEntity
+		{
+			return Context.Set<TEntity>().FromSqlInterpolated(sql);
+		}
+		public int ExecuteSqlRaw(string sql, params object[] parameters)
+		{
+			return Context.Database.ExecuteSqlRaw(sql, parameters);
+		}
+		public void BulkMerge<TEntity>(List<TEntity> entities) where TEntity : DomainEntity
+		{
+			Context.BulkMerge<TEntity>(entities);
+		}
+		public void BulkMerge<TEntity>(List<TEntity> entities, Action<BulkMergeOptions<TEntity>> options) where TEntity : DomainEntity
+		{
+			Context.BulkMerge<TEntity>(entities, options);
+		}
+		public void BulkInsert<TEntity>(List<TEntity> entities) where TEntity : DomainEntity
+		{
+			Context.BulkInsert<TEntity>(entities);
+		}
+		public void BulkDelete<TEntity>(List<TEntity> entities) where TEntity : DomainEntity
+		{
+			Context.BulkDelete<TEntity>(entities);
+		}
+	}
 }
