2e5509d6fc850846a93b1d04d744279bebb78066
Italo Silveira <italobrian@gmail.com>
2024-06-18 13:06:13 -0300

    v8.0.6.1

--- src/BAYSOFT.Abstractions/Core/Domain/Interfaces/Infrastructures/Data/IWriter.cs
+++ src/BAYSOFT.Abstractions/Core/Domain/Interfaces/Infrastructures/Data/IWriter.cs
@@ -1,4 +1,7 @@
 ﻿using BAYSOFT.Abstractions.Core.Domain.Entities;
+using BAYSOFT.Abstractions.EntityFrameworkCore.Extensions;
+using System.Collections.Generic;
+using System;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -20,5 +23,11 @@ namespace BAYSOFT.Abstractions.Core.Domain.Interfaces.Infrastructures.Data
         public void RemoveRange<TEntity>(params TEntity[] entities) where TEntity : DomainEntity;
         public int Commit();
         public Task<int> CommitAsync(CancellationToken cancellationToken = default);
-    }
+        public IQueryable<TEntity> FromSqlInterpolated<TEntity>(FormattableString sql) where TEntity : DomainEntity;
+        public int ExecuteSqlRaw(string sql, params object[] parameters);
+		public void BulkMerge<TEntity>(List<TEntity> entities) where TEntity : DomainEntity;
+		public void BulkMerge<TEntity>(List<TEntity> entities, Action<BulkMergeOptions<TEntity>> options) where TEntity : DomainEntity;
+		public void BulkInsert<TEntity>(List<TEntity> entities) where TEntity : DomainEntity;
+		public void BulkDelete<TEntity>(List<TEntity> entities) where TEntity : DomainEntity;
+	}
 }
