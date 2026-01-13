8e66edfcce72542d1748d2d5f1b80d529280f4e2
Ítalo Silveira <italobrian@gmail.com>
2026-01-12 23:55:11 -0300

    10.0.1.1

---
+++ src/BAYSOFT.Abstractions.Tools/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace BAYSOFT.Abstractions.Crosscutting.Extensions
+{
+	public static class StringExtensions
+	{
+		public static string ToCamelCase(this string value)
+		{
+			return System.Text.Json.JsonNamingPolicy.CamelCase.ConvertName(value);
+		}
+		public static string ToPascalCase(this string value)
+		{
+			var newValue = value;
+			
+			foreach (var part in value.Split(' ', '_', '-'))
+			{
+				if (part.Length > 0)
+				{
+					newValue = newValue.Replace(part, char.ToUpper(part[0]) + part.Substring(1).ToLower());
+				}
+			}
+
+			return newValue;
+		}
+	}
+}
